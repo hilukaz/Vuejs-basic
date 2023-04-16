650e0071897034aeff7fdd8d812d91239a6e10c3
hilukaz <lucaskawakami5@gmail.com>
2023-04-16 08:38:39 -0300

    emit

--- src/components/Dinamic.vue
+++ src/components/Dinamic.vue
@@ -2,15 +2,38 @@
     <img v-bind:src="avatar" alt="">
     <!--v-bind:src ou href//// : simplificado-->
     <!--atributo dinâmico, ele pode ser um link href, ou imagem-->
+    <Emit @trocarImagem="mudarImagem"/>
+    <!-- a emissão do evento trocar imagem ativou o mudar imagem -->
 </template>
 
 <script>
+import Emit from "./emit.vue";
     export default{
         name:'DinamicP',
         data(){
             return{
-                avatar:'/favicon.ico'
+                avatar:'/favicon.ico',
+                boolean:1
+            }
+        },
+        components:{
+            Emit
+        },
+        methods:{
+            mudarImagem(){
+                if (this.boolean===1) {
+                    this.avatar='/favicn.ico'
+                    this.boolean=0
+                }else{
+                    this.avatar='/favicon.ico'
+                    this.boolean=1
+                }
             }
         }
     }
 </script>
+<style scoped>
+    img{
+        max-width: 30px;
+    }
+</style>
---
+++ src/components/emit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="$emit('trocarImagem')">trocar imagem</button>
+    <!-- emit= comunicação filho pro pai -->
+    <!-- props= comunicação pai pro filho -->
+  </div>
+</template>
+
+<script>
+export default {
+  name:'EmitP',
+  emits:['trocarImagem']
+  
+};
+</script>
+
+<style></style>
